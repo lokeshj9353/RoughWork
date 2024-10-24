c2e5338c30c77fdd6d225ec9bfbd168bc230fcf0
lokeshj9353 <lokeshlokij1122@gmail.com>
2024-10-24 10:02:50 +0530

    i committed 2nd Class

---
+++ RoughWork/src/com/java/NewClass.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Added new class into the Repository ");
+	}
+
+}
